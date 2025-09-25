4028140121afeaf79c3f649553db420709e5051f
owlsaver <michaelwacey@msn.com>
2025-09-25 11:48:18 -0400

    Changes

--- src/main/java/com/github/owlsaver3/App.java
+++ src/main/java/com/github/owlsaver3/App.java
@@ -2,12 +2,13 @@ package com.github.owlsaver3;
 
 /**
  * Hello world!
+ * This is a great program
+ * It works really well
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Have a nice day.");
     }
 }
